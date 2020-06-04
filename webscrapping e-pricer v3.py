e2fc1e42d5ace7a5ec3e252a699b5bf4cd4db68d
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2020-06-04 15:12:10 -0300

    removida linhas testes para ajustar a tabela

--- webscrapping e-pricer v3.py
+++ webscrapping e-pricer v3.py
@@ -9,36 +9,44 @@ soup = BeautifulSoup(open(html_doc), 'html.parser')
 config_table = soup.find_all('table', id = "componentDetailsTable")[3].find_all('table')[0]
 #b = type(config_table) <class 'bs4.element.Tag'> c = len(config_table)
 totrow = len(config_table.find_all('tr'))
-print(totrow)
 count = 0
-count1 = 0
-Product = 0
 tabela = []
 row_index = 0
-app_colum = 0
 for row in config_table.find_all('tr'):
     if count not in (0, 1, 2, totrow - 1):
-        tag = config_table.find_all('tr')[count]
-        if tag.get_attribute_list('bgcolor')[0] == '#CCCCCC': count1 += 1
-        #print(count1)
         tabela.append([])
         for header in row.find_all('th'):
-            if header.text.strip() == 'Product': Product += 1
             tabela[row_index].append(header.text.strip())
             for cell in row.find_all('td'):
                 tabela[row_index].append(cell.text.strip())
-        print(len(tabela))
         row_index += 1    
         count += 1
     else:
         count += 1
-print(Product)        
-print(tabela)
-#print(len(tabela))
-#df = pd.DataFrame(data=tabela[1:] ,columns=(tuple(tabela[0])))
-#df['Quantity'] = df['Quantity'].str.replace(',','.',regex=True) #replace commad by dot
-#print(df)
-#df.to_excel("testv2.xlsx",index=False)
-#df1 = df
-#print(df1)
+df = pd.DataFrame(data=tabela[1:] ,columns=(tuple(tabela[0])))
+df['Quantity'] = df['Quantity'].str.replace(',','.',regex=True) #replace commad by dot
+print(df)
+
+'''
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter("testv4.xlsx")
+# Convert the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='pricer',index=False)
+# Get the xlsxwriter workbook and worksheet objects.
+Workbook  = writer.book
+worksheet = writer.sheets['pricer']
+# Add some cell formats.
+format1 = Workbook.add_format({'num_format': '0.0'})
+format2 = Workbook.add_format({'num_format': '0'})
+
+
+# Set the column width and format.
+worksheet.set_column('E:E', 18, format2)
+
+# Set the format but not the column width.
+worksheet.set_column('C:C', None)
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
+'''
 
