a12b20e38ef2235c3deb39577d132d3dd73ad098
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2020-06-20 02:19:12 -0300

    webscrapping table 4, para clicar na pagina

--- webscrapping table 4 espn exemplo duas tabela selenium.py
+++ webscrapping table 4 espn exemplo duas tabela selenium.py
@@ -1,16 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
+import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 url = 'https://www.espn.com/nba/stats/player/_/season/2017/seasontype/2/table/offensive/sort/avgPoints/dir/desc'
 
 driver = webdriver.Firefox()
-driver.implicitly_wait(10)
+driver.implicitly_wait(3)
 driver.get(url)
 
 #response = requests.get(url)
+
+last = None
+while not last:
+    try:
+        last = driver.find_element_by_xpath(
+            "//table[@class='Table Table--align-right Table--fixed Table--fixed-left']/tbody/tr[271]/td").text
+        # print(last)
+    except NoSuchElementException:
+        time.sleep(2)
+        continue_link = driver.find_element_by_link_text(
+            'Show More')  # print(continue_link.text)
+        continue_link.click()
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 stat_table = soup.find_all('table', class_ = 'Table Table--align-right Table--fixed Table--fixed-left') #nome e rank de jogador
@@ -41,3 +55,5 @@ rank = pandas1()
 df = pd.DataFrame(rank[1:], columns=(tuple(rank[0])))
 df.to_excel("natalia_linda4.xlsx",index=False)
 print('acabou')
+
+driver.close()
